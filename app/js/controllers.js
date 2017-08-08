349205a2d89b0075cbe0b5708c64b7eec8a68090
kimia shz <shzkimia@gmail.com>
2017-08-08 11:31:19 +0430

    remove phone list controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,17 +1,2 @@
 /* sample angular project - controllers js */
 
-sampleApp
-    .controller('PhoneController', function ($scope) {
-        $scope.phones = [
-            {
-                name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.'
-            }, {
-                name: 'SAMSUNG Galaxy s8 plus',
-                snippet: 'The Next, Next Generation tablet.'
-            }, {
-                name: 'MOTOROLA XOOM',
-                snippet: 'The Next, Next Generation tablet.'
-            }
-        ];
-    });
