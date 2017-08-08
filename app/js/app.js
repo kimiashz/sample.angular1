39b920b70b8cf30115b4a47dc1a812804cca4f7d
kimia shz <shzkimia@gmail.com>
2017-08-08 11:43:35 +0430

    move config in app.js

--- app/js/app.js
+++ app/js/app.js
@@ -5,3 +5,29 @@ window.application = {
 };
 
 var sampleApp = angular.module('sampleApp', ['ngRoute', 'ngMaterial', 'ngAria', 'ngResource']);
+
+angular.module('sampleApp')
+    .config(function ($routeProvider) {
+        'use strict';
+
+        var routeConfig = {
+            controller: 'TodoCtrl',
+            templateUrl: 'todomvc-index.html',
+            resolve: {
+                store: function (todoStorage) {
+
+                    return todoStorage.then(function (module) {
+                        module.get();
+                        return module;
+                    });
+                }
+            }
+        };
+
+        $routeProvider
+            .when('/', routeConfig)
+            .when('/:status', routeConfig)
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/js/config.js
+++ app/js/config.js
@@ -1,27 +1,2 @@
 /* sample angular project - config js */
 
-angular.module('sampleApp')
-    .config(function ($routeProvider) {
-        'use strict';
-
-        var routeConfig = {
-            controller: 'TodoCtrl',
-            templateUrl: 'todomvc-index.html',
-            resolve: {
-                store: function (todoStorage) {
-
-                    return todoStorage.then(function (module) {
-                        module.get();
-                        return module;
-                    });
-                }
-            }
-        };
-
-        $routeProvider
-            .when('/', routeConfig)
-            .when('/:status', routeConfig)
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
