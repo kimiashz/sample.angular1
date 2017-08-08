3f22b7cf6b8e85a5d154fef0673d3d5a8f1ca617
kimia shz <shzkimia@gmail.com>
2017-08-08 11:34:31 +0430

    create directives todo mvc

---
+++ app/js/directives.js
@@ -0,0 +1,33 @@
+/* sample angular project - directives js */
+
+sampleApp
+	.directive('todoEscape', function () {
+		'use strict';
+
+		var ESCAPE_KEY = 27;
+
+		return function (scope, elem, attrs) {
+			elem.bind('keydown', function (event) {
+				if (event.keyCode === ESCAPE_KEY) {
+					scope.$apply(attrs.todoEscape);
+				}
+			});
+
+			scope.$on('$destroy', function () {
+				elem.unbind('keydown');
+			});
+		};
+	})
+	.directive('todoFocus', function todoFocus($timeout) {
+		'use strict';
+
+		return function (scope, elem, attrs) {
+			scope.$watch(attrs.todoFocus, function (newVal) {
+				if (newVal) {
+					$timeout(function () {
+						elem[0].focus();
+					}, 0, false);
+				}
+			});
+		};
+	});
